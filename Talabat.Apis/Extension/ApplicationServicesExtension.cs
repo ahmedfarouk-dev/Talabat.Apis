261477b45536b17e03e5b329e1bfc8d31bff762e
ahmedfarouk22277@gmail.com <ahmedfarouk22277@gmail.com>
2024-12-27 22:42:45 +0200

    AddApplicationServices

---
+++ Talabat.Apis/Extension/ApplicationServicesExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talabat.Apis.Errors;
+using Talabat.Core.Interfaces;
+using Talabat.Repositories;
+
+namespace Talabat.Apis.Extension
+{
+    public static class ApplicationServicesExtension
+    {
+
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddScoped(typeof(IRepositories<>), typeof(GenericRepository<>));
+
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState.Where(p => p.Value.Errors.Count() > 0)
+                    .SelectMany(p => p.Value.Errors).Select(p => p.ErrorMessage).ToList();
+                    var response = new ApiValidationErrorHandling()
+                    {
+
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(response);
+                };
+            });
+
+            return services;
+        }
+    }
+}
--- Talabat.Apis/Program.cs
+++ Talabat.Apis/Program.cs
@@ -1,10 +1,6 @@
-
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Talabat.Apis.Errors;
+using Talabat.Apis.Extension;
 using Talabat.Apis.Helpers;
-using Talabat.Core.Interfaces;
-using Talabat.Repositories;
 using Talabat.Repositories.Data;
 using Talabat.Repositories.DataSeeding;
 
@@ -29,25 +25,9 @@ namespace Talabat.Apis
 
                            option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
             );
-
-            builder.Services.AddScoped(typeof(IRepositories<>), typeof(GenericRepository<>));
             builder.Services.AddAutoMapper(p => p.AddProfile(new MappingProfiles(builder.Configuration)));
 
-            builder.Services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = context =>
-                {
-                    var errors = context.ModelState.Where(p => p.Value.Errors.Count() > 0)
-                    .SelectMany(p => p.Value.Errors).Select(p => p.ErrorMessage).ToList();
-                    var response = new ApiValidationErrorHandling()
-                    {
-
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(response);
-                };
-            });
-
+            builder.Services.AddApplicationServices();
             var app = builder.Build();
             #endregion
 
